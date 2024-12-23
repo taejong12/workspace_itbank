0823f8aed86fa187dc29e7b36b96c4d9620c8516
dbxow <dbxow@taejong>
2024-12-23 14:46:24 +0900

    pro01

---
+++ pro01/src/ex01/Hello.java
@@ -0,0 +1,26 @@
+package ex01;
+
+// 한줄 주석 : 주석은 프로그램의 실행과는 상관없이 설명을 쓸 수 있다.
+/*
+ * 여러줄 주석
+ * 1. 설명
+ * 2. 내용
+ * 3. 목차를 사용 할 수 있다. 
+ */
+/**
+ * API 주석
+ * - 기능 사용 설명을 위한 주석
+ */
+
+// class 의 시작
+public class Hello { 
+
+	// method 의 시작
+	public static void main(String[] args) {
+		// method 의 실행 명령
+		System.out.println("Hello, "
+				+ "welcome to javaworld");
+		
+	}// method 의 끝
+
+} // class 의 끝
---
+++ pro01/src/ex01/Hello1.java
@@ -0,0 +1,15 @@
+package ex01;
+
+public class Hello1 {
+	public static void main(String[] args) {
+		// 기본적으로 method 에 실행 명령 코드를 입력
+		// 변수 - 데이터를 저장하는 공간
+		// int : 정수 자료형 명시
+		// num : 데이터를 저장하는 공간의 이름
+		// 100 : 저장할 값
+		int num = 100;
+		
+		// 출력문 - 모니터에 문자열을 출력하도록 하는 명령
+		System.out.println(num);
+	}
+}
